fc12dbe844f1eda4cc52d5a48ba1b9cc87c1f3b8
Joseph Platta <jwplatta@gmail.com>
2024-11-18 00:09:39 -0600

    util function to sending https get requests

---
+++ src/utils/httpsGET.ts
@@ -0,0 +1,33 @@
+import { request } from 'https';
+
+export function httpsGET(url: string, hostname: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: hostname,
+      path: url,
+      method: 'GET',
+      headers: {
+        'User-Agent': 'jwplatta@gmail.com', // TODO: grab from settings.
+        'Access-Control-Allow-Origin': '*'
+      }
+    };
+
+    const req = request(options, (res) => {
+      let data = '';
+
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      res.on('end', () => {
+        resolve(data);
+      });
+    });
+
+    req.on('error', (error) => {
+      reject(error);
+    });
+
+    req.end();
+  });
+}
