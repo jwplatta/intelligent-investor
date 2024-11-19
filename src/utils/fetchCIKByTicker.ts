dac37e618140dc8fd9b77adb95deb8a61bc66e14
Joseph Platta <jwplatta@gmail.com>
2024-11-18 21:08:19 -0600

    add email param to fetchCIKByTicker

--- src/utils/fetchCIKByTicker.ts
+++ src/utils/fetchCIKByTicker.ts
@@ -3,8 +3,8 @@ import Company from '@/src/types/Company';
 
 const COMPANY_TICKERS_URL = "https://www.sec.gov/files/company_tickers.json";
 
-export async function fetchCIKByTicker(ticker: string) {
-  const data = await httpsGET(COMPANY_TICKERS_URL, 'www.sec.gov');
+export async function fetchCIKByTicker(ticker: string, email: string) {
+  const data = await httpsGET(COMPANY_TICKERS_URL, 'www.sec.gov', email);
   const parsedData: { [key: string]: Company } = JSON.parse(data);
   const company = Object.values(parsedData).find((item: Company) => item.ticker === ticker.toUpperCase()) as Company;
 
