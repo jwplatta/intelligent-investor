9a2968e233b83dfbcc0d27814fe26330c8295097
Joseph Platta <jwplatta@gmail.com>
2024-11-18 21:08:31 -0600

    add email param to fetchCompanyCIKs

--- src/utils/fetchCompanyCIKs.ts
+++ src/utils/fetchCompanyCIKs.ts
@@ -3,8 +3,8 @@ import Company from '@/src/types/Company';
 
 const COMPANY_TICKERS_URL = "https://www.sec.gov/files/company_tickers.json";
 
-export async function fetchCompanyCIKs() {
-  const data = await httpsGET(COMPANY_TICKERS_URL, 'www.sec.gov');
+export async function fetchCompanyCIKs(email: string) {
+  const data = await httpsGET(COMPANY_TICKERS_URL, 'www.sec.gov', email);
   const parsedData: { [key: string]: Company } = JSON.parse(data);
   const companyList: Company[] = Object.values(parsedData);
 
