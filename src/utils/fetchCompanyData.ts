bf8323eb93da5ad72f184d32810ac18edc3b5634
Joseph Platta <jwplatta@gmail.com>
2024-11-18 21:08:45 -0600

    add email param to fetchCompanyData

--- src/utils/fetchCompanyData.ts
+++ src/utils/fetchCompanyData.ts
@@ -3,9 +3,9 @@ import { CompanyData } from '@/src/types/CompanyData';
 
 const COMPANY_FACTS_URL = 'https://data.sec.gov/api/xbrl/companyfacts/CIK';
 
-export async function fetchCompanyData(cik: string) {
+export async function fetchCompanyData(cik: string, email: string) {
   const url = `${COMPANY_FACTS_URL}${"0".repeat(10 - cik.toString().length)}${cik}.json`;
-  const data = await httpsGET(url, 'data.sec.gov');
+  const data = await httpsGET(url, 'data.sec.gov', email);
   const parsedData: CompanyData = JSON.parse(data) ;
   return parsedData;
 }
